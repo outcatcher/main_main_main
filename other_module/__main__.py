163fef8aa3973cc45a901ee91b6b53eb8c62dee5
Anton Kachurin <anton.kachurin@mc2soft.ru>
2022-07-04 21:23:51 +0300

    A few more details

--- other_module/__main__.py
+++ other_module/__main__.py
@@ -3,8 +3,9 @@ try:
 
     print(f"Тут нормально: {__name__} (__main__.py)")
 
-except ImportError:
+except ImportError as e:
     print(f"А тут - немного по-другому: {__name__} (__main__.py)")
+    print(f"Тут была ошибка: {e.msg}")
 
     from other_module.main1 import here
 
